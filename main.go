b551226a94b0c39ab375a0626062092a517de5a9
luminous44 <aklemasultanakhi@gmail.com>
2025-11-06 08:05:25 +0600

    added hello handeler

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+func helloHander(res http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(res,"404 not found",http.StatusNotFound)
+	}
+
+	if r.Method != "GET"{
+		http.Error(res,"Method id not supported",http.StatusNotFound)
+	}
+
+	fmt.Fprintf(res,"Hello")
+}
+
+
 func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
