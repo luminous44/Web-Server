fed217fc918b139285db70fd1b8dd2953f2ce7b5
luminous44 <aklemasultanakhi@gmail.com>
2025-11-06 08:15:34 +0600

    added form handler

--- main.go
+++ main.go
@@ -5,15 +5,38 @@ import (
 	"log"
 	"net/http"
 )
+func formHandler(res http.ResponseWriter, r *http.Request){
 
-func helloHander(res http.ResponseWriter, r *http.Request){
+		if r.URL.Path != "/home" {
+		http.Error(res,"404 not found",http.StatusNotFound)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+
+		fmt.Fprintf(res,"ParseForm() err: %v",err)
+		return
+		
+	}
+	fmt.Fprintf(res,"Post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(res,"Name : %s\n",name)
+	fmt.Fprintf(res,"Address : %s\n",address)
+
+}
+
+func helloHandler(res http.ResponseWriter, r *http.Request){
 	if r.URL.Path != "/hello" {
 		http.Error(res,"404 not found",http.StatusNotFound)
+		return
 	}
 
 	if r.Method != "GET"{
 		http.Error(res,"Method id not supported",http.StatusNotFound)
-	}
+		return
+ 	}
 
 	fmt.Fprintf(res,"Hello")
 }
@@ -23,8 +46,8 @@ func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
-	http.HandleFunc("/home",formHander)
-	http.HandleFunc("/hello",helloHander)
+	http.HandleFunc("/home",formHandler)
+	http.HandleFunc("/hello",helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 
