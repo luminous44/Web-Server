9a3d08e83f2e5103a95858c2ebd311c615e6ad48
luminous44 <aklemasultanakhi@gmail.com>
2025-11-06 08:00:36 +0600

    created roud

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/",fileServer)
+	http.HandleFunc("/home",formHander)
+	http.HandleFunc("/hello",helloHander)
+
+	fmt.Printf("Starting server at port 8080\n")
+
+	if err := http.ListenAndServe(":8080",nil); err != nil{
+      log.Fatal(err)
+	}
+
+
+
 }
